cf688e20708cdc2b957d47f855b1d062d21d30a2
DEUK CHEON, OH <guruwang2@gmail.com>
2021-10-02 05:29:06 +0900

    타입스크립트로 대체 및 레이아웃

---
+++ web/pages/index.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+import style from "./index.module.scss";
+
+const Home: NextPage = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const state: any = {
+    ctx: null,
+  };
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    state.ctx = ctx;
+    console.log(ctx);
+  }, []);
+  return (
+    <div>
+      <Head>
+        <title>AniTool</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <section className={style.container}>
+          <section id={style.menuTool}>메뉴바</section>
+          <section id={style.commandTool}>명령어아이콘</section>
+          <section id={style.drawScreen}>
+            <canvas ref={canvasRef} height={600} width={400}></canvas>
+          </section>
+          <section id={style.sideTool}>사이드툴</section>
+          <section id={style.animationTool}>애니메이션툴</section>
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
